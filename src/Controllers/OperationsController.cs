9cac4ec51a18a07152627c9c141fd0663cca6a55
Pedro Guerrato <pguerrato@gmail.com>
2019-08-08 17:59:25 -0300

    Created functional API

---
+++ src/Controllers/OperationsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+
+namespace CoreCodeCamp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public OperationsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        // GET: api/values
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)this.configuration;
+
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
